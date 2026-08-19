c657411fdc3cdf0c8deb97cec27222f3ca097aed
EMILIO PABLO CERRATE <epcerrate@gmail.com>
2026-08-19 01:12:11 -0300

    Desarrollo de los gráficos de la página 2 y creación del mapa para mostrar las coordenadas de una cuenta.

--- streamlit/package/funciones.py
+++ streamlit/package/funciones.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from st_constantes import DATA_PATH
+import folium
 
 def carga_de_datos():
     """
@@ -9,8 +10,54 @@ def carga_de_datos():
     Returns:
         df (dataframe): Dataframe con toda la información de SIGAF.
     """
-    file_path = DATA_PATH / "Base Consolidada 2.xlsx"
+    file_path = DATA_PATH / "Base Consolidada.xlsx"
     if not file_path.exists():
         raise FileNotFoundError("No se encontró el archivo \"Base Consolidada.xlsx\".")
     df = pd.read_excel(file_path, sheet_name="SIGAF")
     return df
+
+def conditions(df):
+    """
+    Separa las condiciones en partidas y cáscaras vacías.
+    Args:
+        df (dataframe): Dataframe con toda la información de SIGAF.
+    Returns:
+        values2 (list): Lista con los valores para el gráfico de distribución de partidas.
+    """
+    con_partida = {"Con partida.", "Partida repetida, esta es la madre.", "Partida repetida, esta es la agregación.", "Revisar. Son varias manzanas con varias partidas."}
+    cantidad_con_partida = df["Condición"].isin(con_partida).sum()
+    return [cantidad_con_partida, len(df) - cantidad_con_partida]
+
+def coordenates(df):
+    """
+    Toma la cantidad de fichas que tienen y no tienen coordenadas.
+    Args:
+        df (dataframe): Dataframe con toda la información de SIGAF.
+    Returns:
+        values3 (list): Lista con los valores para el gráfico de distribución de coordenadas.
+    """
+    cantidad_con_coordenadas = df["Coordenadas"].count()
+    return [cantidad_con_coordenadas, len(df) - cantidad_con_coordenadas]
+
+def generar_mapa():
+    """
+    Genera el mapa para mostrar las fichas con coordenadas.
+    Args:
+        None.
+    Returns:
+        m (map): Mapa para las fichas con coordenadas.
+    """
+    attr = (
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> '
+    'contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
+    )
+    tiles = 'https://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0/capabaseargenmap@EPSG%3A3857@png/{z}/{x}/{-y}.png'
+    m = folium.Map(
+        location=(-36.6769, -60.5588),
+        control_scale=True,
+        zoom_start=6,
+        name='es',
+        tiles=tiles,
+        attr=attr
+    )
+    return m
--- streamlit/pages/02_Análisis_por_cuenta.py
+++ streamlit/pages/02_Análisis_por_cuenta.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from st_constantes import DATA_PATH
-from package.funciones import carga_de_datos
+from package.funciones import carga_de_datos, conditions, coordenates, generar_mapa
 import matplotlib.pyplot as plt
+from streamlit_folium import st_folium
 
 st.title("Análisis por cuenta")
 
@@ -18,21 +19,46 @@ st.markdown("""
 
 # Elección de cuenta
 cuentas = df["Cuenta Unificada"].unique()
-cuenta = st.selectbox("Cuenta", cuentas)
+cuenta = st.selectbox("Cuenta", cuentas, index=None)
 
 if cuenta:
     try:
+        # Filtro el dataframe por cuenta
         df_reducido = df[(df["Cuenta Unificada"] == cuenta)]
-        values = df["Condición"].value_counts().values
-        labels = df["Condición"].value_counts().index
+
+        st.subheader("Comparación partidas, Distribución por condición y Cantidad con coordenadas")
+
+        # Creamos los tres gráficos
+        fig, ax = plt.subplots(3, 1, figsize=(12, 18))
+        
         # Gráfico 1
-        st.subheader("Comparación partidas")
-        plt.figure(figsize=(10, 5))
-        plt.pie(values, labels=labels, autopct='%1.2f%%')
-        plt.title("Comparación partidas")
-        st.pyplot()
+        values = df_reducido["Condición"].value_counts().values
+        labels = df_reducido["Condición"].value_counts().index
+        ax[0].pie(values, labels=labels, 
+                  autopct=lambda p: '{:.1f}%\n({:.0f})'.format(p, p * sum(values) / 100))
+        ax[0].set_title("COMPARACIÓN PARTIDAS")
 
         # Gráfico 2
-        st.subheader("Distribución por condición")
+        values2 = conditions(df_reducido)
+        labels2 = ["Con partida", "Cáscara vacía"]
+        ax[1].pie(values2, labels=labels2, colors=["green", "red"],
+                  autopct=lambda p: '{:.1f}%\n({:.0f})'.format(p, p * sum(values2) / 100))
+        ax[1].set_title("DISTRIBUCIÓN POR CONDICIÓN")
+        
+        # Gráfico 3
+        values3 = coordenates(df_reducido)
+        labels3 = ["Tienen coordenadas", "No tienen coordenadas"]
+        ax[2].pie(values3, labels=labels3, colors=["green", "red"],
+                  autopct=lambda p: '{:.1f}%\n({:.0f})'.format(p, p * sum(values2) / 100))
+        ax[2].set_title("COMPARACIÓN COORDENADAS")
+
+        # Ajustamos los gráficos y los mostramos
+        fig.tight_layout()
+        st.pyplot(fig)
+
+        # Mapa para las partidas con coordenadas
+        st.subheader("Mapa con las coordenadas")
+        mapa = generar_mapa()
+        st_folium(mapa)
     except:
         st.warning("No existe información para la cuenta seleccionada.")
